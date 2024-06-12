dd34572e097c5a0007269556ad6b61e61c41c1c9
InsafNilam <insafnilam.2000@gmail.com>
2024-06-12 15:45:39 +0530

    feat: Add @radix-ui/react-scroll-area component for scrollable areas
    This commit adds the @radix-ui/react-scroll-area component to the project. This component provides a scrollable area for content, allowing users to scroll through long or overflowing content. It enhances the user experience by providing a smooth and intuitive scrolling experience. The component is used in the ScrollArea component to create a scrollable viewport for the content. This addition improves the usability and functionality of the application.

--- resources/js/Pages/Document/Index.jsx
+++ resources/js/Pages/Document/Index.jsx
@@ -1,15 +1,51 @@
+import { Button } from "@/Components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/Components/ui/dialog";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/react";
-import React from "react";
+import React, { useState } from "react";
+import FileUpload from "./Partials/FileUpload";
 
 export default function Index({ auth, documents }) {
+  const [open, setOpen] = useState(false);
+  const [files, setFiles] = useState({});
+
   return (
     <AuthenticatedLayout
       user={auth.user}
       header={
-        <h2 className="font-semibold text-xl text-gray-800 leading-tight">
-          Dashboard
-        </h2>
+        <div className="flex justify-between">
+          <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+            Document
+          </h2>
+          <Dialog open={open} onOpenChange={setOpen}>
+            <DialogTrigger asChild>
+              <Button className="px-4 py-2 cursor-pointer text-sm rounded-full font-medium leading-5 text-white transition-colors duration-150 bg-blue-600 border border-transparent active:bg-blue-600 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue">
+                Upload
+              </Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px] md:max-w-screen-md">
+              <DialogHeader>
+                <DialogTitle>Upload Documents</DialogTitle>
+                <DialogDescription>
+                  Securely store your documents in your workspace for easy and
+                  safe access.
+                </DialogDescription>
+              </DialogHeader>
+              <FileUpload files={files} setFiles={setFiles} />
+              <DialogFooter>
+                <Button type="submit">Upload</Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        </div>
       }
     >
       <Head title="Documents" />
---
+++ resources/js/Pages/Document/Partials/FileUpload.jsx
@@ -0,0 +1,142 @@
+import React, { useState } from "react";
+import FileTemplate from "./FileTemplate";
+import { ScrollArea } from "@/Components/ui/scroll-area";
+
+export default function FileUpload({ files, setFiles }) {
+  const [counter, setCounter] = useState(0);
+  const [isDraggedOver, setIsDraggedOver] = useState(false);
+
+  const addFile = (file) => {
+    const objectURL = URL.createObjectURL(file);
+    const fileData = {
+      name: file.name,
+      size: file.size,
+      type: file.type,
+      objectURL,
+    };
+
+    setFiles((prevFiles) => ({ ...prevFiles, [objectURL]: fileData }));
+  };
+
+  const handleFiles = (files) => {
+    for (const file of files) {
+      addFile(file);
+    }
+  };
+
+  const handleFileUpload = (e) => {
+    handleFiles(e.target.files);
+  };
+
+  const handleFileDelete = (objectURL) => {
+    setFiles((prevFiles) => {
+      const updatedFiles = { ...prevFiles };
+      delete updatedFiles[objectURL];
+      return updatedFiles;
+    });
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    handleFiles(e.dataTransfer.files);
+    setIsDraggedOver(false);
+    setCounter(0);
+  };
+
+  const handleDragEnter = (e) => {
+    e.preventDefault();
+    if (e.dataTransfer.types.includes("Files")) {
+      setCounter((prevCounter) => prevCounter + 1);
+      setIsDraggedOver(true);
+    }
+  };
+
+  const handleDragLeave = (e) => {
+    setCounter((prevCounter) => prevCounter - 1);
+    if (counter <= 1) {
+      setIsDraggedOver(false);
+    }
+  };
+
+  const handleDragOver = (e) => {
+    if (e.dataTransfer.types.includes("Files")) {
+      e.preventDefault();
+    }
+  };
+  return (
+    <div className="flex flex-col gap-2">
+      <article
+        aria-label="File Upload Modal"
+        className={`w-full p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 py-8 flex flex-col justify-center items-center ${
+          isDraggedOver ? "" : ""
+        }}`}
+        onDrop={handleDrop}
+        onDragOver={handleDragOver}
+        onDragLeave={handleDragLeave}
+        onDragEnter={handleDragEnter}
+      >
+        <p className="mb-3 font-semibold text-gray-900 flex flex-wrap justify-center">
+          <span>Drag and drop your</span>&nbsp;
+          <span>files anywhere or</span>
+        </p>
+        <input
+          id="file"
+          type="file"
+          multiple
+          className="hidden"
+          onChange={(e) => handleFileUpload(e)}
+        />
+        <label
+          htmlFor="file"
+          className="cursor-pointer flex items-center justify-center flex-col"
+        >
+          <svg
+            className="w-12 h-12 text-gray-400"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+            ></path>
+          </svg>
+          <span className="text-base leading-normal">Select files</span>
+        </label>
+      </article>
+      <div className="space-y-2">
+        <h1 className="font-semibold sm:text-lg text-gray-900">To Upload</h1>
+        <ScrollArea className="h-44 w-full">
+          <div className="h-full w-full pr-3">
+            {Object.keys(files).length === 0 ? (
+              <div className="text-center flex flex-col items-center justify-center">
+                <img
+                  className="mx-auto w-32"
+                  src="https://user-images.githubusercontent.com/507615/54591670-ac0a0180-4a65-11e9-846c-e55ffce0fe7b.png"
+                  alt="no data"
+                />
+                <span className="text-small text-gray-500">
+                  No files selected
+                </span>
+              </div>
+            ) : (
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3">
+                {Object.values(files).map((file) => (
+                  <div key={file.objectURL} className="p-1 h-40">
+                    <FileTemplate
+                      file={file}
+                      handleFileDelete={() => handleFileDelete(file.objectURL)}
+                    />
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        </ScrollArea>
+      </div>
+    </div>
+  );
+}

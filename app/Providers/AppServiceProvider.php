415120d991f71d52607b6fa9785aa5954bf864ff
miminthikhaff github <mhdinthikaff@gmail.com>
2024-07-09 12:55:55 +0530

    feat: Register PdfService and PdfManager for generating PDFs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,10 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use App\Services\FileManager;
 use App\Services\FileService;
+use App\Services\PdfManager;
+use App\Services\PdfService;
+
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,7 +21,14 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton(FileService::class, function ($app) {
             return FileService::getInstance($app->make(FileManager::class));
         });
+
+        $this->app->singleton(PdfService::class, function ($app) {
+            return PdfService::getInstance($app->make(PdfManager::class));
+        });
+
     }
+
+
 
     /**
      * Bootstrap any application services.

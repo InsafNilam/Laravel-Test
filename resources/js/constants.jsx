fb04364eb517f0afd020c01ea0301c30cf713577
miminthikhaff github <mhdinthikaff@gmail.com>
2024-07-09 12:50:46 +0530

    refactor: Add constants for course, batch, and centre status maps

---
+++ resources/js/constants.jsx
@@ -0,0 +1,42 @@
+export const COURSE_STATUS_CLASS_MAP = {
+    verified: "green",
+    pending: "yellow",
+    under_review: "blue",
+    approved: "green",
+    rejected: "red",
+    discontinued: "red",
+};
+export const COURSE_STATUS_TEXT_MAP = {
+    verified: "Verified",
+    under_review: "Under Review",
+    approved: "Approved",
+    rejected: "Rejected",
+    discontinued: "Discontinued",
+    pending: "Pending",
+};
+export const BATCH_STATUS_CLASS_MAP = {
+    active: "green",
+    inactive: "red",
+};
+export const BATCH_STATUS_TEXT_MAP = {
+    active: "Active",
+    inactive: "In Active",
+};
+export const CENTRE_STATUS_CLASS_MAP = {
+    application: "green",
+    onboarding: "yellow",
+    under_review: "blue",
+    pending: "orange",
+    approved: "blue",
+    rejected: "red",
+    discontinued: "pink",
+};
+export const CENTRE_STATUS_TEXT_MAP = {
+    application: "Application",
+    onboarding: "Onboarding",
+    under_review: "Under_review",
+    pending: "Pending",
+    approved: "Approved",
+    rejected: "Rejected",
+    discontinued: "Discontinued",
+};
